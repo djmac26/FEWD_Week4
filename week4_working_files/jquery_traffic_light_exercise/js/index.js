17da60fab8158790d713213cfac032a13d3e27b0
djmac26 <dotymacabeo26@gmail.com>
2016-01-21 23:41:20 +0800

    Week 4 assignment - in progress

--- week4_working_files/jquery_traffic_light_exercise/js/index.js
+++ week4_working_files/jquery_traffic_light_exercise/js/index.js
@@ -1,1 +1,24 @@
 //Implement the red light using jQuery. Don't forget to add the script tags.
+
+//Vanilla JS
+// document.getElementByID("stopButton").onclick = illuminateRed;
+
+//Jquery
+$("#stopButton").click(illuminateRed);
+// $("#slowButton").click(illuminateYellow);
+// $("#goButton").click(illuminateGreen);
+
+function illuminateRed() {
+  clearLights();
+
+  $("#stopLight").css("backgroundColor", "red");
+  // $("slowLight").css("backgroundColor", "yellow");
+  // $("goLight").css("backgroundColor", "green");
+}
+
+function clearLights() {
+
+  $("#stopLight").css("backgroundColor", "black");
+  // $("slowLight").css("backgroundColor", "black");
+  // $("goLight").css("backgroundColor", "black");
+}
